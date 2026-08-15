21a34bd0f3f67545367883583ff72df50884d74a
Kim Pedersen <kim_p@get2net.dk>
2026-08-15 13:06:14 +0200

    Initial Commit

---
+++ BrickView/BrickViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrickViewApp.swift
+//  BrickView
+//
+//  Created by Kim Pedersen on 15/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct BrickViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
